8ee5f7b14c236c9eaaf47c3bb63c063dce43e62a
jantapabh <jantapa2407@gmail.com>
2022-06-19 20:49:35 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import dynamic from 'next/dynamic'
+
+const AblyChatComponent = dynamic(() => import('../components/AblyChatComponent'), { ssr: false });
+
 
 export default function Home() {
   return (
@@ -15,6 +19,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <AblyChatComponent />
 
         <p className={styles.description}>
           Get started by editing{' '}
